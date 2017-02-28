f5237c6e287ba4e234a07f2f97881300d67eee4f
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-28 10:20:42 +0100

    Make DB read and write functions public cause they're used in tests

--- db.py
+++ db.py
@@ -1,23 +1,23 @@
 import json
 
 
-def _read():
+def read():
     from server import app
     with open(app.config["DB_NAME"], "r") as f:
         return json.loads(f.read())
 
 
-def _write(data):
+def write(data):
     from server import app
     with open(app.config["DB_NAME"], "w") as f:
         f.write(json.dumps(data))
 
 
 def get(key):
-    return _read()[key]
+    return read()[key]
 
 
 def put(key, value):
-    data = _read()
+    data = read()
     data[key] = value
-    _write(data)
+    write(data)
